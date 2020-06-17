d9e0cf4386e21a483d6ef1ed3d0f404a7f397a1e
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-06-17 09:57:35 -0300

    Exercício 04 da lista 1

---
+++ Exercicio04_lista1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lista1_Exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a nota dos 3 primeiros bimestres:");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = (600 - (n1 * 2 + n2 * 2 + n3 * 3)) / 3;
+            Console.WriteLine($"Nota para passar: {n4}");
+            Console.ReadKey();
+        }
+    }
+}
