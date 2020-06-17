e82303d8d3315c8792f3092472220fcc1f38dec7
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-06-17 09:53:14 -0300

    Exercício02_lista1

---
+++ Exercicio01_Lista1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lista1_Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome:");
+            String nome = Console.ReadLine();
+            Console.WriteLine($"Bem-vindo ao C#, {nome}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exericio02_lista1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lista1_Exercício02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite as notas dos dois bimestres da disciplina:");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int media = (2 * n1 + 3 * n2) / 5;
+            Console.WriteLine($"Media Parcial = {media}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
