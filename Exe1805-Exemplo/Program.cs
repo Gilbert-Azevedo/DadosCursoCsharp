adff6625042c4a9c7eb9e4e0b586e7654008264a
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-06-17 18:27:21 -0300

    Exercicio 1805 URI

---
+++ Exe1805-Exemplo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exe1805_Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, n;
+            long soma;
+            string s = Console.ReadLine();
+            string[] v = s.Split(" ");
+            a = int.Parse(v[0]);
+            b = int.Parse(v[1]);
+            n = b - a + 1;
+            soma = ((long)a + b)*n / 2;
+           Console.WriteLine(soma);
+           Console.ReadKey();
+
+        }
+    }
+}
