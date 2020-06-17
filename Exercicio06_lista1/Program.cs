988d15b3ce91c599618abd55959bad7ee37c0462
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-06-17 10:14:53 -0300

    Exercício 06 da lista 1

---
+++ Exercicio06_lista1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio06_lista1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a Base e a Altura de um Retângulo");
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double area = b * h;
+            double perim = 2*b + 2 * h;
+            double diag = Math.Sqrt(b * b + h * h);
+            Console.WriteLine($"Area = {area} - Perimetro = {perim} - Diagonal = {diag}");
+            Console.ReadKey();
+
+        }
+    }
+}
