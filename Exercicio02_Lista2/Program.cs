47b3567c0b3af127fdbb2b5b8a5bfeddd0607771
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-06-18 09:35:22 -0300

    Exercício02_Lista2

---
+++ Exercicio02_Lista2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Exercicio02_Lista2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite quatro valores inteiros");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = int.Parse(Console.ReadLine());
+
+            double media = (n1+n2+n3+n4)/4;
+
+            Console.WriteLine($" Média = {media}");
+            Console.WriteLine("Números menores que a média");
+            if (n1 < media)
+            {
+            Console.WriteLine(n1);
+            }
+            if (n2 < media)
+            {
+            Console.WriteLine(n2);
+            }
+            if (n3 < media)
+            {
+            Console.WriteLine(n3);
+            }
+            if (n4 < media)
+            {
+            Console.WriteLine(n4);
+            }
+
+            Console.WriteLine("Números maiores que a média");
+            if (n1 >= media)
+            {
+                Console.WriteLine(n1);
+            }
+            if (n2 >= media)
+            {
+                Console.WriteLine(n2);
+            }
+            if (n3 >= media)
+            {
+                Console.WriteLine(n3);
+            }
+            if (n4 >= media)
+            {
+                Console.WriteLine(n4);
+            }
+            Console.ReadKey();
+        }
+    }
+}
