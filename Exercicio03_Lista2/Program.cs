a156e26145535e1f7cd312abdee362c636addfe0
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-06-18 09:51:18 -0300

    Exercicio03_Lista2

---
+++ Exercicio03_Lista2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exercicio03_Lista2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int somapar = 0,somaimpar=0;
+            Console.WriteLine("Digite quatro valores inteiros");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = int.Parse(Console.ReadLine());
+
+            if (n1 % 2 == 0)
+            {
+                somapar += n1;
+            }
+            else
+            {
+                somaimpar += n1;
+            }
+            if (n2 % 2 == 0)
+            {
+                somapar += n2;
+            }
+            else
+            {
+                somaimpar += n2;
+            }
+            if (n3 % 2 == 0)
+            {
+                somapar += n3;
+            }
+            else
+            {
+                somaimpar += n3;
+            }
+            if (n4 % 2 == 0)
+            {
+                somapar += n4;
+            }
+            else
+            {
+                somaimpar += n4;
+            }
+
+            Console.WriteLine($"Soma dos números pares = {somapar}");
+            Console.WriteLine($"Soma dos números impares = {somaimpar}");
+            Console.ReadKey();
+        }
+    }
+}
