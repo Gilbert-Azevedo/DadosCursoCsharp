dbdc697d47fea9dc470043083296a38f50d9ad40
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-06-22 08:48:45 -0300

    .

--- ExercicioURI1021/Program.cs
+++ ExercicioURI1021/Program.cs
@@ -7,35 +7,34 @@ namespace ExercicioURI1021
         static void Main(string[] args)
         {
             string novovalor = Console.ReadLine();
-
-            string[] nvalor = novovalor.Split('.');
-            int parteint = int.Parse(nvalor[0]);
-            int partefrac = int.Parse(nvalor[1]);
+             string[] nvalor = novovalor.Split('.');
+             int parteint = int.Parse(nvalor[0]);
+             int partefrac = int.Parse(nvalor[1]);
                  
              int n100 = parteint/100;
              parteint = parteint%100;
              int n50 = parteint/50;
              parteint = parteint% 50;
-            int n20 = parteint/20;
+             int n20 = parteint/20;
              parteint = parteint%20;
-            int n10 = parteint/10;
+             int n10 = parteint/10;
              parteint = parteint%10;
-            int n5 = parteint/5;
+             int n5 = parteint/5;
              parteint = parteint%5;
-            int n2 = parteint/2;
+             int n2 = parteint/2;
              parteint =parteint%2;
-            int m1 = parteint;
+             int m1 = parteint;
                
                                
-             int m05 = partefrac /50;
+             int m05 = partefrac / 50;
              partefrac = partefrac % 50;
-            int m025 = partefrac /25;
-             partefrac = partefrac %25;
-            int m01 = partefrac / 10;
+             int m025 = partefrac / 25;
+             partefrac = partefrac % 25;
+             int m01 = partefrac / 10;
              partefrac = partefrac % 10;
-            int m005 = partefrac / 5;
+             int m005 = partefrac / 5;
              partefrac = partefrac % 5;
-            int m001 = partefrac;
+             int m001 = partefrac;
                
                Console.WriteLine("NOTAS:");
                Console.WriteLine($"{n100} nota(s) de R$ 100.00");
