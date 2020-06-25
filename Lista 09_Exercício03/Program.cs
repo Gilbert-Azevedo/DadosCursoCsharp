9edd64c9a49a7a6652758e535f0bdcb8191f8029
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-06-25 15:24:17 -0300

    Lista 09 exercicio 06

---
+++ Lista 09_Exercício03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lista_09_Exercício03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o valor da base do retangulo"); 
+            double b1= double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor da altura do retangulo");
+            double h1 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"A diagonal do retângulo é :{Diagonal(b1,h1):00.00} m2");
+            Console.ReadKey();
+
+        }
+
+        public static double Diagonal(double b, double h)
+        {
+            double diag = Math.Sqrt(Math.Pow(b, 2)+ Math.Pow(h, 2)) ;
+            return diag;
+        }
+    }
+}
