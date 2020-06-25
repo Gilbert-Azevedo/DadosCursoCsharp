fed13c9f62ddb2bdd1d923eb4d0f0ed43c0b69c9
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-06-25 16:17:31 -0300

    Lista09_Exercício08

---
+++ Lista09_Exercicio08/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lista09_Exercicio08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Digite um numero Real");
+            double num = double.Parse(Console.ReadLine());
+            Console.WriteLine($" O menor inteiro é: {MenorInteiro(num)}");
+            Console.ReadKey();
+
+        }
+
+
+        public static int MenorInteiro(double x)
+        {
+
+            int xint;
+            if (x > (int)Math.Round(x))
+            {
+               xint = (int)Math.Round(x) + 1;
+            }
+            else
+               xint = (int)Math.Round(x);
+
+            return xint;
+         }
+    }
+}
