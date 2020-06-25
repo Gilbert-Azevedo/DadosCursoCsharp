7f05aec7f386a3317e086ed80c55764f5c990121
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-06-25 11:41:56 -0300

    Exercicios lista 9

---
+++ Exercicio06_Lista6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercicio06_Lista6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, cont=3;
+            int[] vetor;
+            
+            vetor = new int[30];
+            vetor[0] = 1;
+            vetor[1] = 2;
+            vetor[2] = 3;
+          //  Console.Write($"{vetor[0]} ");
+           // Console.Write($"{vetor[1]} ");
+           // Console.Write($"{vetor[2]} ");
+            
+            for (i = 3; i < 29; i++)
+            {
+                if (cont == 3)
+                {
+                    vetor[i] = vetor[i - 1] + vetor[i - 2] + vetor[i - 3];
+                    cont = 0;
+                    
+                }
+                else
+                { 
+                vetor[i] = i;
+                cont++;
+                }
+               
+                Console.Write($"{vetor[i]} ");
+            }
+                          
+             
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Lista09/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lista09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois números reais");
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"o maior dentre eles é {Maior(n1,n2)}");
+            Console.ReadKey();
+
+
+        }
+        public static double Maior(double x, double y)
+        {
+            if (x > y)
+            {
+                return x;
+            }
+            else
+            {
+                return y;
+            }
+
+        
+        }
+
+
+    }
+}
