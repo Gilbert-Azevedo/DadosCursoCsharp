e6dda6af0f9bd8b1c575f6170c8a79db9810396e
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-06-30 17:45:40 -0300

    Exercicios da lista 11

---
+++ Lista11/Exercicio02/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio02
+{
+    class Disciplina
+    {
+        public string nome;
+        public double nota1, nota2, nota3, nota4,notafinal, mediaparc, mediaf;
+        public double MediaParcial()
+        {
+            mediaparc = (2 * nota1 + 2 * nota2 + 3 * nota3 + 3 * nota4) / 10;
+            return mediaparc;
+        }
+        public double MediaFinal()
+        {
+            mediaf = (mediaparc + notafinal) /2;
+            return mediaf;
+
+        }
+
+    }
+}
