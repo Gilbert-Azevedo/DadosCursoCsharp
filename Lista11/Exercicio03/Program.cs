de4a882a40786ee12b5e468e9d2121132b3136db
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-06-30 18:10:02 -0300

    Inclusão do exercicio3 da lista 11

---
+++ Lista11/Exercicio03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Viagem trip = new Viagem();
+            Console.WriteLine("Digite a distância percorrida em Km");
+            trip.dist = double.Parse(Console.ReadLine());
+            Console.WriteLine("informe a quantidade de total de horas:");
+            trip.horas = double.Parse(Console.ReadLine());
+            Console.WriteLine("informe a quantidade total de minutos:");
+            trip.min = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"A velocidade média da viagem foi de {trip.Velocidade():0.00} Km/h");
+           
+            Console.ReadKey();
+
+        }
+    }
+}
