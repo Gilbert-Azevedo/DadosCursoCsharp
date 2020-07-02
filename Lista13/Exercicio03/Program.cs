8b284eda1b58adf9f646162e90836508f44f450a
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-07-02 19:41:02 -0300

    Lista 13 - Inclusão da questão 3

---
+++ Lista13/Exercicio03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Informe o valor do numero inteiro a ser convertido");
+            int numero = int.Parse(Console.ReadLine());
+            Conversor conv = new Conversor(numero);
+            Console.WriteLine(conv.ToString());
+            Console.WriteLine("Informe outro numero inteiro a ser convertido");
+            numero = int.Parse(Console.ReadLine());
+            conv.SetNum(numero);
+            Console.WriteLine(conv.ToString());
+
+
+            Console.ReadKey();
+        }
+    }
+}
