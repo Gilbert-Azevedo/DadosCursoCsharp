be5a73bd16884515b9e6866bada9ef7c515e78d9
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-07-02 17:30:41 -0300

    Inclusão do exercicio 02

---
+++ Lista13/Exercicio01/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    class Retangulo
+    {
+        private double b,h;
+        public Retangulo(double b, double h) {
+            if (b > 0) this.b = b;
+            if (h > 0) this.h = h;
+            }
+        public void SetBase(double b)
+        {
+            if (b > 0) this.b = b;
+        }
+        public void SetAltura(double h)
+        {
+            if (h > 0) this.h = h;
+        }
+
+        public double GetBase(double b)
+        {
+            return this.b;
+        }
+        public double GetAlgura(double h)
+        {
+            return this.b;
+        }
+
+        public double CalcArea()
+        {
+            double area = b * h;
+            return area;
+        }
+        public double CalcDiagonal()
+        {
+            double diagonal = Math.Sqrt(b*b+h*h);
+            return diagonal;
+        }
+        public override string ToString()
+        {
+            return $"Base = {b}  - Altura = {h}";
+        }
+
+    }
+}
---
+++ Lista13/Exercicio02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a Distancia percorrida em Km");
+            double dist = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o peso da carga em kg");
+            double pes = double.Parse(Console.ReadLine());
+
+            Frete frt = new Frete(dist, pes);
+            Console.WriteLine($"o valor do frete é : R$ {frt.CalcFrete():0.00} ");
+            Console.WriteLine(frt.ToString());
+
+            Console.WriteLine("Informe a nova Distancia percorrida em Km");
+            dist = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o novo peso da carga em kg");
+            pes = double.Parse(Console.ReadLine());
+            frt.SetDistancia(dist);
+            frt.SetPeso(pes);
+            Console.WriteLine($"o novo valor do frete é : R$ {frt.CalcFrete():0.00} ");
+            Console.WriteLine(frt.ToString());
+            Console.ReadKey();
+        }
+    }
+}
