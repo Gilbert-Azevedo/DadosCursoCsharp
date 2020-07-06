cc6ae0a721940074e63ee44c8d1444b0b45d6347
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-07-06 15:24:05 -0300

    Lista14 - Inclusao do Exercicio 03

---
+++ Lista14/Exercicio03/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03
+{
+    class Boleto
+    {
+        private string codBarras;
+        private DateTime dateEmissao = new DateTime();
+        private DateTime dateVencimento = new DateTime();
+        private DateTime datePagto = new DateTime();
+        private decimal valorBoleto, valorPago;
+        private Pagamento situacaoPagamento;
+
+        public Boleto(string cod, DateTime emissao, DateTime venc, DateTime datepag, decimal valor)
+        {
+            if (cod != null) codBarras = cod;
+            dateEmissao = emissao;
+            dateVencimento = venc;
+            datePagto = datepag;
+            if (valor > 0) valorBoleto = valor;
+
+        }
+
+        public void Pagar(decimal valor)
+        {
+            if (valor <= valorBoleto) valorPago = valor;
+        }
+
+          public Pagamento Situacao()
+        {
+            situacaoPagamento = Pagamento.EmAberto;
+
+            if ((valorPago > 0) && (valorPago < valorBoleto))
+            {
+                situacaoPagamento = Pagamento.PagoParcial;
+            }
+            else
+            {
+                if (valorPago == valorBoleto) situacaoPagamento = Pagamento.Pago;
+            }
+
+            return situacaoPagamento;
+        }
+      
+
+
+       public enum Pagamento {
+            
+            EmAberto, PagoParcial, Pago
+        }
+
+        public override string ToString()
+        { 
+          return $" Código de Barras :  {codBarras}  - Data de Emissão : {dateEmissao:dd/MM/yyyy} - Data de Vencimento : {dateVencimento:dd/MM/yyyy} " +
+                $"- Data de Pagamento : {datePagto:MM / dd / yyyy}  - Valor do Boleto (R$) = {valorBoleto:0.00} - Valor Pago (R$) = {valorPago:0.00} " +
+                $"- Situação: {Situacao()}";
+        }
+
+
+    }
+}
---
+++ Lista14/Exercicio03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string codB = "1234353.4544643.43425433424";
+            DateTime dataemis = DateTime.Parse("01/07/2020") ;
+            DateTime datavenc = dataemis.AddDays(15);
+            DateTime datapagto  = DateTime.Now;
+            Console.WriteLine("informe o valor do Boleto:");
+            decimal vBoleto = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe o valor a ser pago");
+            decimal valorPg = decimal.Parse(Console.ReadLine());
+
+            Boleto boletoapagar = new Boleto(codB, dataemis, datavenc, datapagto, vBoleto);
+            boletoapagar.Pagar(valorPg);
+           
+            Console.WriteLine(boletoapagar.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
