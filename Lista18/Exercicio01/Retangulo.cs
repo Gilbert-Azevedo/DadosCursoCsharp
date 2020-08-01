919143b2f83ad03cec69971c65d5be6aef2893ae
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-08-01 10:06:31 -0300

    Lista18 - Exercicio 1 V.1

---
+++ Lista18/Exercicio01/Retangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    class Retangulo
+    {
+        private double b, h;
+        public Retangulo(double b, double h)
+        {
+            if (b > 0) this.b = b;
+            if (h > 0) this.h = h;
+        }
+        public double Base { get => b; set => b = value > 0 ? value : 0; }
+        public double Altura { get => h; set => h = value > 0 ? value : 0; }
+        public double Area { get => b*h;}
+        public double Diagonal { get => Math.Sqrt(b*b + h*h);}
+
+      /* public override string ToString()
+       {
+           return $" {Base} | Altura = {Altura}";
+       }*/
+
+
+
+    }
+}
