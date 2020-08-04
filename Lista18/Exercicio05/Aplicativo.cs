848bdbfb5524439854590923a45fbb931cf88171
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-08-04 10:15:44 -0300

    Lista18 - Atualização

---
+++ Lista18/Exercicio05/Aplicativo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio05
+{
+    class Aplicativo
+    {
+        
+       private int curtidas;
+
+     
+     
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public string Preco { get; set; }
+        public int Curtidas { get { return curtidas; } }
+
+        public void Curtir()
+        {
+            curtidas++;
+        }
+
+        public override string ToString()
+        {
+            return $" Nome do aplicativo:   {Nome}  | Categoria:  {Categoria} |  Preço:  {Preco}";
+        }
+
+    }
+}
---
+++ Lista18/Exercicio05/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercicio05
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Loja novaloja = new Loja();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Inserir_Click(object sender, RoutedEventArgs e)
+        {
+            Aplicativo novo = new Aplicativo();
+            novo.Nome = txtNome.Text;
+            novo.Categoria = txtCateg.Text;
+            novo.Preco = txtPreco.Text;
+            novaloja.Inserir(novo);
+           Listapps.ItemsSource = novaloja.Listar();
+        }
+
+        private void Excluir_Click(object sender, RoutedEventArgs e)
+        {
+            if (Listapps.SelectedItem == null)
+                MessageBox.Show("Nenhum Item Selecionado!");
+            else
+            {
+                Aplicativo app = (Aplicativo)Listapps.SelectedItem;
+                novaloja.Excluir(app);
+                Listapps.ItemsSource = novaloja.Listar();
+            }
+        }
+
+        private void Pesquisar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Curtir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+               
+        private void Listar_Click(object sender, RoutedEventArgs e)
+        {
+           Listapps.ItemsSource = novaloja.Listar();
+        }
+    }
+}
