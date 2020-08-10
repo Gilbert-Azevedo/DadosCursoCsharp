23c3f4e696df1d80ee3bf6f9ec19063a2e7c24dd
Jose Alvaro de Paiva <alvaro.paiva@academico.ifrn.edu.br>
2020-08-10 08:52:30 -0300

    Lista19 - Atualização exe2

---
+++ Lista19/Exercicio02/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercicio02
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Playlist novaplay = new Playlist();  
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NovaPlayList_Click(object sender, RoutedEventArgs e)
+        {
+            novaplay.Nome = txtNome.Text;
+            novaplay.Descricao = txtDescricao.Text;
+        }
+
+        private void Inserir_Click(object sender, RoutedEventArgs e)
+        {
+            Musica novamusica = new Musica();
+            novamusica.Titulo = txtTitulo.Text;
+            novamusica.Artista = txtArtista.Text;
+            novamusica.Album = txtAlbum.Text;
+            novamusica.Duracao = TimeSpan.Parse(txtDuracao.Text);
+            novaplay.Inserir(novamusica);
+            ListaMusicas.ItemsSource = novaplay.Listar();
+        }
+
+        private void Listar_Click(object sender, RoutedEventArgs e)
+        {
+            ListaMusicas.ItemsSource = novaplay.Listar();
+        }
+
+        private void TempoTotal_Click(object sender, RoutedEventArgs e)
+        {
+            txtDuracTotal.Text = novaplay.TempoTotal().ToString();
+        }
+    }
+}
---
+++ Lista19/Exercicio02/Musica.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio02
+{
+    class Musica
+    {
+        private string titulo, artista, album;
+        private DateTime dataInclusao;
+        private TimeSpan duracao = new TimeSpan();
+
+        public string Titulo {
+
+            get { return titulo; }
+            set { if (value != null) titulo = value; }
+                   
+        }
+        public string Artista
+        {
+            get { return artista; }
+            set { if (value != null) artista = value; }
+        }
+        public string Album
+        {
+            get { return album; }
+            set { if (value != null) album = value; }
+        }
+        public TimeSpan Duracao
+        {
+            get { return duracao; }
+            set { if (value != null) duracao = value; }
+        }
+        public DateTime DataInclusao
+        {
+            get { return DateTime.Now; }
+        }
+       
+
+        public override string ToString()
+        {
+            return $" Musica: {Titulo } - Artista: {Artista } - Descrição: {Album} - Data de Inclusao {DataInclusao:dd/MM/yyyy}  - Duracao {Duracao} ";
+        }
+        public void SetDataInclusao(DateTime d)
+        {
+            dataInclusao = d;
+        }
+       
+    }
+}
